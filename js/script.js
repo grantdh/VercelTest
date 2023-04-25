0599491144343eaca0809126298283731a361904
grantdh <grantdheileman@gmail.com>
2023-04-25 11:56:39 -0600

    mandelbrot bbanner test

--- js/script.js
+++ js/script.js
@@ -35,3 +35,41 @@ images.forEach((img) => {
 });
 
 setInterval(scrollGallery, 50);
+
+
+function mandelbrotASCII() {
+  const width = 80;
+  const height = 22;
+  const maxIterations = 1000;
+  const mandelbrot = document.getElementById("mandelbrot");
+  let output = "";
+
+  for (let y = 0; y < height; y++) {
+    for (let x = 0; x < width; x++) {
+      const zx = (x / width) * 3.5 - 2.5;
+      const zy = (y / height) * 2 - 1;
+
+      let c = { x: zx, y: zy };
+      let z = { x: 0, y: 0 };
+      let iterations = 0;
+
+      while (iterations < maxIterations) {
+        const xt = z.x * z.x - z.y * z.y + c.x;
+        z.y = 2 * z.x * z.y + c.y;
+        z.x = xt;
+        iterations++;
+
+        if (z.x * z.x + z.y * z.y > 4) {
+          break;
+        }
+      }
+
+      output += iterations === maxIterations ? "#" : " ";
+    }
+    output += "\n";
+  }
+
+  mandelbrot.textContent = output;
+}
+
+mandelbrotASCII();
