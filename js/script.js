6bb244f11abe4d8cca500ad0102c6c16578b1012
grantdh <grantdheileman@gmail.com>
2023-04-25 12:21:40 -0600

    CGPT .JS overhaul

--- js/script.js
+++ js/script.js
@@ -1,49 +1,17 @@
 const gallery = document.querySelector(".gallery");
-const images = document.querySelectorAll(".gallery img");
 
-let scrollDirection = 1;
-let scrollSpeed = 1;
-
-function scrollGallery() {
-  gallery.style.transform = `translateX(${scrollDirection * scrollSpeed}px)`;
-  scrollSpeed += 1;
-
-  // Reset the scroll speed when the mouse is not over the gallery
-  setTimeout(() => {
-    if (!gallery.matches(":hover")) {
-      scrollSpeed = 1;
-    }
-  }, 50);
-}
-
-images.forEach((img) => {
-  img.addEventListener("mouseover", () => {
-    scrollDirection = 0;
-  });
-
-  img.addEventListener("mouseout", (event) => {
-    const mouseX = event.clientX;
-    const imgRect = img.getBoundingClientRect();
-
-    // Determine the scroll direction based on the mouse position
-    if (mouseX < imgRect.left) {
-      scrollDirection = -1;
-    } else {
-      scrollDirection = 1;
-    }
-  });
-});
-
-setInterval(scrollGallery, 50);
-
-let scrollSpeed = 1;
+let scrollSpeed = 0;
+let lastMouseX = null;
 
 document.addEventListener("mousemove", (e) => {
   const windowWidth = window.innerWidth;
   const mouseX = e.clientX;
 
-  // Calculate the scroll speed based on the cursor's position
-  scrollSpeed = (mouseX / windowWidth) * 2 - 1;
+  if (lastMouseX !== null) {
+    scrollSpeed = mouseX - lastMouseX;
+  }
+
+  lastMouseX = mouseX;
 });
 
 function updateGalleryPosition() {
@@ -54,10 +22,12 @@ function updateGalleryPosition() {
   // Update the gallery's position
   gallery.style.transform = `translateX(${newTranslateX}px)`;
 
+  // Reduce scroll speed gradually
+  scrollSpeed *= 0.95;
+
   // Continue updating the gallery's position
   requestAnimationFrame(updateGalleryPosition);
 }
 
 // Start updating the gallery's position
-updateGalleryPosition();
-
+requestAnimationFrame(updateGalleryPosition);
