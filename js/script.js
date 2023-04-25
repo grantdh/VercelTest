711a2e5a478c1ef8772519991e3b7d8052e7b1d2
grantdh <grantdheileman@gmail.com>
2023-04-25 10:17:03 -0600

    AAdded scroll gallery to landing page and img folder

--- js/script.js
+++ js/script.js
@@ -1,7 +1,37 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const button = document.getElementById('click-me');
-    
-    button.addEventListener('click', function() {
-        alert('Button clicked!');
-    });
+const gallery = document.querySelector(".gallery");
+const images = document.querySelectorAll(".gallery img");
+
+let scrollDirection = 1;
+let scrollSpeed = 1;
+
+function scrollGallery() {
+  gallery.style.transform = `translateX(${scrollDirection * scrollSpeed}px)`;
+  scrollSpeed += 1;
+
+  // Reset the scroll speed when the mouse is not over the gallery
+  setTimeout(() => {
+    if (!gallery.matches(":hover")) {
+      scrollSpeed = 1;
+    }
+  }, 50);
+}
+
+images.forEach((img) => {
+  img.addEventListener("mouseover", () => {
+    scrollDirection = 0;
+  });
+
+  img.addEventListener("mouseout", (event) => {
+    const mouseX = event.clientX;
+    const imgRect = img.getBoundingClientRect();
+
+    // Determine the scroll direction based on the mouse position
+    if (mouseX < imgRect.left) {
+      scrollDirection = -1;
+    } else {
+      scrollDirection = 1;
+    }
+  });
 });
+
+setInterval(scrollGallery, 50);
