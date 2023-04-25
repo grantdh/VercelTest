11019faa9cf8186bdb4a188f600832efaf7a0b23
grantdh <grantdheileman@gmail.com>
2023-04-25 02:25:19 -0600

    Added HTML, CSS, and JS

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('click-me');
+    
+    button.addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+});
