51dc7c1a92e9a1e4287282db9ff65bcefcaf731d
grantdh <grantdheileman@gmail.com>
2023-04-25 19:51:19 -0600

    might haave messed it uup here

--- js/script.js
+++ js/script.js
@@ -1,87 +1,53 @@
-const gallery = document.querySelector(".gallery");
-const images = Array.from(gallery.children);
+const canvas = document.getElementById('patternCanvas');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-let scrollSpeed = 0;
-let lastMouseX = null;
-let hoveringImage = false;
-let mousePosition = { x: null, y: null };
-
-images.forEach((image) => {
-  // Add a click event listener to each image to navigate to the close-up view page.
-  // Replace '#' with the actual URL for the close-up view page for each image.
-  image.addEventListener("click", () => {
-    window.location.href = '#';
-  });
-
-  image.addEventListener("mouseenter", (e) => {
-    hoveringImage = true;
-    scrollSpeed = 0;
-    image.style.transform = `translateX(${image._translateX}px) scale(1.2)`;
-    image.style.zIndex = "10";
-  });
-
-  image.addEventListener("mouseleave", (e) => {
-    hoveringImage = false;
-    image.style.transform = `translateX(${image._translateX}px) scale(1)`;
-    image.style.zIndex = "1";
-  });
-});
-
-document.addEventListener("mousemove", (e) => {
-  const windowWidth = window.innerWidth;
-  const mouseX = e.clientX;
-  const centerY = window.innerHeight / 2;
-  const centerX = windowWidth / 2;
-
-  mousePosition.x = mouseX; // Update mouse position
-  mousePosition.y = centerY; // Update mouse position
-
-  if (!hoveringImage) {
-    if (lastMouseX !== null) {
-      scrollSpeed = (mouseX - centerX) / 50;
-    }
+class Polygon {
+  constructor(x, y, sides, radius, angle) {
+    this.x = x;
+    this.y = y;
+    this.sides = sides;
+    this.radius = radius;
+    this.angle = angle;
   }
 
-  lastMouseX = mouseX;
-});
+  draw() {
+    ctx.beginPath();
+    ctx.moveTo(this.x + this.radius * Math.cos(this.angle), this.y + this.radius * Math.sin(this.angle));
 
-function updateGalleryPosition() {
-  const mouseX = mousePosition.x;
-  const mouseY = mousePosition.y;
-
-  images.forEach((image) => {
-    const rect = image.getBoundingClientRect();
-    const isInsideImage = mouseX > rect.left && mouseX < rect.right && mouseY > rect.top && mouseY < rect.bottom;
-
-    if (isInsideImage) {
-      if (!hoveringImage) {
-        image.dispatchEvent(new Event("mouseenter"));
-      }
-    } else {
-      if (hoveringImage) {
-        image.dispatchEvent(new Event("mouseleave"));
-      }
+    for (let i = 1; i <= this.sides; i++) {
+      ctx.lineTo(
+        this.x + this.radius * Math.cos(this.angle + i * 2 * Math.PI / this.sides),
+        this.y + this.radius * Math.sin(this.angle + i * 2 * Math.PI / this.sides)
+      );
     }
-  });
 
-  if (!hoveringImage) {
-    images.forEach((image, index) => {
-      const imageWidth = image.offsetWidth;
-      const currentTranslateX = image._translateX || 0;
-      let newTranslateX = currentTranslateX + scrollSpeed;
-
-      if (newTranslateX > window.innerWidth + imageWidth / 2) {
-        newTranslateX -= (window.innerWidth + imageWidth) * 2;
-      } else if (newTranslateX < -imageWidth - imageWidth / 2) {
-        newTranslateX += (window.innerWidth + imageWidth) * 2;
-      }
-
-      image._translateX = newTranslateX;
-      image.style.transform = `translateX(${newTranslateX}px) scale(1)`;
-    });
+    ctx.closePath();
+    ctx.stroke();
   }
-
-  requestAnimationFrame(updateGalleryPosition);
 }
 
-requestAnimationFrame(updateGalleryPosition);
+function animate() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  const polygons = [];
+  const sides = [3, 4, 6];
+  const angle = Math.random() * Math.PI * 2;
+
+  for (let i = 0; i < 50; i++) {
+    const x = Math.random() * canvas.width;
+    const y = Math.random() * canvas.height;
+    const radius = Math.random() * 100;
+    const sideIndex = Math.floor(Math.random() * sides.length);
+
+    polygons.push(new Polygon(x, y, sides[sideIndex], radius, angle));
+  }
+
+  polygons.forEach((polygon) => {
+    polygon.draw();
+  });
+
+  requestAnimationFrame(animate);
+}
+
+animate();
